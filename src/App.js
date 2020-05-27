21f9118711b9b7668c3d240e434a132e45dbf8c5
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-05-27 20:47:27 +0200

    Create Books Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Books from './Components/Books'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Books />
     </div>
   );
 }
---
+++ src/Components/Books.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Books extends Component {
+    render() {
+        return (
+            <div className="books">
+                Books
+            </div>
+        )
+    }
+}
+
+export default Books
