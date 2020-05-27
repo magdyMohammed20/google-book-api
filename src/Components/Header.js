87b5ae056c04b050760651f15c3600f11dedf384
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-05-27 10:45:17 +0200

    Creating Header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                <i className="fa fa-book"></i>
+                Book Cards
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
